be63e2b98c2b32b63bc4ae5cd459725b185a8917
bamssaye <bamssayef@gmail.com>
2024-11-07 02:21:41 +0100

    check ReadMe

--- pars/check.c
+++ pars/check.c
@@ -6,7 +6,7 @@
 /*   By: bamssaye <bamssaye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 02:18:11 by bamssaye          #+#    #+#             */
-/*   Updated: 2024/11/07 02:18:40 by bamssaye         ###   ########.fr       */
+/*   Updated: 2024/11/07 02:20:52 by bamssaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,5 @@
 int start_fun(){
 //straaaaaat ....    
 }
+git config --global user.name "bamssaye"
+git config --global user.email "bamssayef@gmail.com"
